5e46e37d98890b5cde2d11350d82789dce2aa948
s-martin <saimen54@hotmail.com>
2020-01-10 22:32:53 +0100

    improved manual login process

--- BingMapsMyPlaces2KML.py
+++ BingMapsMyPlaces2KML.py
@@ -12,7 +12,7 @@ chrome_options.add_argument("--start-maximized")
 # set a profile which is used every time to keep login data
 chrome_options.add_argument("user-data-dir=./chromeprofile")
 
-browser = webdriver.Chrome(chrome_options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options)
 browser.get('https://www.bing.com/maps/myplaces')
 
 kml = simplekml.Kml()
@@ -21,6 +21,24 @@ n = 0
 g = 0
 
 locations = browser.find_elements_by_class_name('bm_favoritesListItem')
+
+# check, if run for the first time and no login data (and therefor no locations available)
+if len(locations) == 0:
+    print('Please login to Bing/Microsoft in the opened Chrome window.')
+    response = input("Please type 'OK', if you have logged in successfully: ")
+    if response != 'OK':
+        print('Please start script again and login')
+        browser.quit()
+        exit()
+
+locations = browser.find_elements_by_class_name('bm_favoritesListItem')
+if len(locations) == 0:
+    print('Could not find any locations')
+    browser.quit()
+    exit()
+
+print('Running conversion...')
+
 for element in locations:
     n = n + 1
     name, address = element.text.splitlines()
--- ReadMe.md
+++ ReadMe.md
@@ -11,8 +11,8 @@ This script scraps *My Places* from [Bing Maps](www.bing.com/maps) and converts 
 3. Download chromedriver from https://chromedriver.chromium.org/downloads, extract and add location of chromedriver binary to `PATH`
 
 ## Usage ##
-1. Run script (ignore error message)
+1. Start script
+    > python BingMapsMyPlaces2KML.py
 2. Login in your Bing/Microsoft account in the opened Chrome(driver) window
-3. Close Chrome window
-4. Run script again
-5. Takes a couple of seconds until locations.kml is saved in working directory
+3. Type `OK` in command line
+4. Takes a couple of seconds until locations.kml is saved in working directory
